4e6c3bc86054c0cedcd39dbc1097e27a69521a76
Jack Pender <20085278@mail.wit.ie>
2021-01-01 18:10:19 +0000

    Week 9 Error Handling

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -16,7 +16,9 @@ router.get('/:id', (req, res, next) => {
 
 router.get('/:id/reviews', (req, res, next) => {
   const id = parseInt(req.params.id);
-  getMovieReviews(id).then(reviews => res.status(200).send(reviews));
+  getMovieReviews(id)
+  .then(reviews => res.status(200).send(reviews))
+  .catch((error) => next(error));
 });
 
 export default router;
